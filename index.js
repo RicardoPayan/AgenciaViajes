ee35fc61ad5b498494ed61a4dbb7981224bf5566
RicardoPayan <a218206581@unison.mx>
2022-08-05 10:55:58 -0700

    cambios en la configuracion del port para que funcione en linux

--- index.js
+++ index.js
@@ -4,7 +4,13 @@ import router from "./routes/index.js";
 const app = express();
 
 //Definir puerto
-const port = process.env.PORT || 4000;
+ 
+let port = 3000;
+const portArg = process.argv[2];
+ 
+if (portArg !== undefined && !Number.isNaN(parseInt(portArg, 10))) {
+  port = parseInt(portArg, 10);
+}
 
 //Habilitar pug
 app.set('view engine','pug');
