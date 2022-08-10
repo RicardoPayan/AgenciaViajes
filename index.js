bb586134f8677009223225bb2316d6944f05c4ea
RicardoPayan <a218206581@unison.mx>
2022-08-09 17:18:30 -0700

    Agregue header y footer dinamico

--- index.js
+++ index.js
@@ -15,6 +15,15 @@ if (portArg !== undefined && !Number.isNaN(parseInt(portArg, 10))) {
 //Habilitar pug
 app.set('view engine','pug');
 
+//Obtener el año actual
+
+app.use((req,res,next)=>{
+  const year = new Date();
+
+  res.locals.actualYear = year.getFullYear();
+  return next();
+});
+
 //Definir la carpeta publica
 app.use(express.static('public'));
 
