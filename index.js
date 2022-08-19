b335dc1feaac20707416f47d52761a9957b91367
RicardoPayan <a218206581@unison.mx>
2022-08-19 16:55:18 -0700

    Creada la base de datos y conectada usando sequelize

--- index.js
+++ index.js
@@ -1,10 +1,18 @@
 import express from "express";
 import router from "./routes/index.js";
+import db from "./config/db.js";
 
 const app = express();
 
-//Definir puerto
- 
+
+
+
+//Conectar a la base de datos
+db.authenticate()
+  .then(() => console.log('Base de datos conectado'))
+  .catch(error => console.log('error'));
+  
+//Definir puerto 
 let port = 3000;
 const portArg = process.argv[2];
  
